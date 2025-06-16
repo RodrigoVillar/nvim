d31a7ea5c7b3545888848243d5e2127a52af57a3
Rodrigo Villar <rodrigojvillarg@gmail.com>
2025-06-15 22:10:00 -0400

    chore: show relevant hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,21 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			pickers = {
+				find_files = {
+					-- Show hidden files (dotfiles)
+					hidden = true,
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+				live_grep = {
+					additional_args = function()
+						return { "--hidden", "--glob", "!**/.git/*" }
+					end,
+				},
+			},
+		},
+	},
 }
