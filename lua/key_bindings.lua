6fbdc6f9c7e73776df0f443314dd09c0c1c82a26
rodrigo <77309055+RodrigoVillar@users.noreply.github.com>
2025-06-20 14:11:57 -0400

    feat: add gitsigns + other qol improvements (#2)
    * feat: add gitsigns
    
    * chore: enable doc autoshow
    
    * chore: change colorscheme
    
    * chore: show relevant hidden files
    
    * chore: various qol improvements

--- lua/key_bindings.lua
+++ lua/key_bindings.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Telescope show recent files" })
 
 vim.keymap.set("n", "<leader>p", "", { desc = "Peek" })
 vim.keymap.set("n", "<leader>pd", builtin.lsp_definitions, { desc = "Peek definitions" })
@@ -14,5 +15,16 @@ vim.keymap.set("n", "<leader>pr", builtin.lsp_references, { desc = "Peek referen
 vim.keymap.set("n", "<leader>n", "", { desc = "NvimTree" })
 vim.keymap.set("n", "<leader>nt", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>g", "", { desc = "Neogit" })
+vim.keymap.set("n", "<leader>g", "", { desc = "Git" })
 vim.keymap.set("n", "<leader>gt", ":Neogit<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { noremap = true, silent = true })
+
+-- Directional window switching (more precise)
+vim.keymap.set("n", "<leader>w", "<C-w>", { desc = "Window Movement" })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Move to window below" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Move to window above" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Move to right window" })
+
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Disable search highlights" })
